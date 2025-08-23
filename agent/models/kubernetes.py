a05772faacad625ef757f446041f90bd72fc782f
LukeMacdonald <82664752+LukeMacdonald@users.noreply.github.com>
2025-08-24 08:50:49 +1000

    added initial code for kubernetes data extraction

---
+++ agent/models/kubernetes.py
@@ -0,0 +1,8 @@
+from kubernetes import client, config
+
+config.load_kube_config()
+
+core_v1 = client.CoreV1Api()
+apps_v1 = client.AppsV1Api()
+batch_v1 = client.BatchV1Api()
+networking_v1 = client.NetworkingV1Api()
